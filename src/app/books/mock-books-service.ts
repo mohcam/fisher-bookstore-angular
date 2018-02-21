8695f2600ad4166df0daacff6519da4e7910a11a
mohcam <mohcam@gmail.com>
2018-02-21 18:51:18 -0500

    Created a new project in angular with template and component using property binding

---
+++ src/app/books/mock-books-service.ts
@@ -0,0 +1,6 @@
+import {Book} from './book';
+export const BOOKS: Book[] = [
+    {id: 1, title: 'Ready Player One', author: 'Ernest cline'},
+    {id: 1, title: ' Catch 22', author: 'Ernest cline'}
+
+];
