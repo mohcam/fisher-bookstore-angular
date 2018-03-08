864bf0dcc0569116454c83930aa1c88c3fefa341
mohcam <mohcam@gmail.com>
2018-03-07 23:46:43 -0500

    Created a new project in angular andconvert our book model into an interface to set up for API integration.

--- src/app/books/mock-books-service.ts
+++
@@ -1,6 +0,0 @@
-import {Book} from './book';
-export const BOOKS: Book[] = [
-    {id: 1, title: 'Ready Player One', author: 'Ernest cline'},
-    {id: 1, title: ' Catch 22', author: 'Ernest cline'}
-
-];
